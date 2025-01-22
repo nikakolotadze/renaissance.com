cd3c30f9977f991a5002a2d54ad4a85d779b2dfe
nikakolotadze <nikankolotadze@gmail.com>
2025-01-22 15:01:18 +0400

    chnnge

--- js/script.js
+++ js/script.js
@@ -1,21 +1,2 @@
-$("#burger-menu").on("click", function(){
-    $("#menu").toggleClass("open-menu");
-    $("#socicon").toggleClass("open-socicon");
-  })
-  
-  
-const audio = document.getElementById('audio');
-
-// თუ არსებობდა მდგომარეობა, დატვირთე
-window.onload = () => {
-  const currentTime = localStorage.getItem('audioCurrentTime');
-  if (currentTime) {
-    audio.currentTime = currentTime;
-  }
-  audio.play();
-};
-
-// შეინახე პოზიცია
-audio.addEventListener('timeupdate', () => {
-  localStorage.setItem('audioCurrentTime', audio.currentTime);
-});
+// Placeholder for future functionality
+console.log("renaissance site loaded!");
