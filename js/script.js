f9fea662652f87f02360911ba076b047123014a8
nikakolotadze <nikankolotadze@gmail.com>
2025-02-02 22:56:02 +0400

    retake

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// Placeholder for future functionality
-console.log("renaissance site loaded!");
-
-document.querySelector('.see-more').addEventListener('click', function() {
-    const hiddenImages = document.querySelectorAll('.hidden');
-    
-    hiddenImages.forEach((image) => {
-      image.classList.remove('hidden');
-    });
-  
-    // დამალეთ ღილაკი
-    this.style.display = 'none';
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("ნიუსების საიტი ჩაიტვირთა!");
+});
